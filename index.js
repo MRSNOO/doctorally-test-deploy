9b99444744781ce1abdc0c1594e1fcfcaee5a2cc
MRSNOO <nnquang.1412@gmail.com>
2020-10-12 23:17:59 +0700

    .

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const bodyParser = require("body-parser")
 const router = require("./api")
 const path = require('path');
 const PORT = process.env.PORT || 5000
+const PASS = process.env.PASS
 
 const app = express();
 
 mongoose.connect(
-  `mongodb+srv://quang:Nguyennhatquang140102@cluster0.4uhkn.mongodb.net/Doctorally?retryWrites=true&w=majority`,
+  `mongodb+srv://quang:${PASS}@cluster0.4uhkn.mongodb.net/Doctorally?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
